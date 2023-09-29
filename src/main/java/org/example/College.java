f2a7a23969961aa56e587313fa8c050d90d745b1
saivelamuri <saivelamuri26@gmail.com>
2023-09-29 22:26:53 +0200

    college added

---
+++ src/main/java/org/example/College.java
@@ -0,0 +1,32 @@
+package org.example;
+
+public class College {
+
+    String students;
+    String courses;
+    String instructors;
+
+    public String getStudents() {
+        return students;
+    }
+
+    public void setStudents(String students) {
+        this.students = students;
+    }
+
+    public String getCourses() {
+        return courses;
+    }
+
+    public void setCourses(String courses) {
+        this.courses = courses;
+    }
+
+    public String getInstructors() {
+        return instructors;
+    }
+
+    public void setInstructors(String instructors) {
+        this.instructors = instructors;
+    }
+}
